3a40fa08d1443300841a920826223c90f886ed77
Harshit-Banwal <harshitbanwal849@gmail.com>
2025-09-20 19:54:45 +0530

    PORT changes

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ const authRoutes = require('./routes/auth');
 const messageRoutes = require('./routes/messages');
 const app = express();
 const socket = require('socket.io');
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
-// mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false);
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -24,6 +25,7 @@ mongoose
     console.log(err.message);
   });
 
+app / get('/', (req, res) => res.status(201).send('API UP...'));
 app.get('/ping', (_req, res) => {
   return res.json({ msg: 'Ping Successful' });
 });
@@ -31,9 +33,7 @@ app.get('/ping', (_req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
-);
+const server = app.listen(port, () => console.log(`Server started on ${port}`));
 const io = socket(server, {
   cors: {
     origin: 'http://localhost:3000',
